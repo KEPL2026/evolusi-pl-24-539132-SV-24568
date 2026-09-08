20a03d0f73542b27f4f911cb6fe7cbe3484d3933
RakanAja05 <muhammadrakanhibrizi2005@mail.ugm.ac.id>
2026-09-08 21:45:18 +0700

    style: rapikan kode menggunakan laravel pint

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,8 +7,8 @@ class ProfileController extends Controller
     public function show()
     {
         $profile = [
-            'nama'  => 'Muhammad Rakan Hibrizi',
-            'nim'   => '24/539132/SV/24568',
+            'nama' => 'Muhammad Rakan Hibrizi',
+            'nim' => '24/539132/SV/24568',
             'kelas' => 'Evolusi Perangkat Lunak',
         ];
 
