692c0f37834ed61323c54f2156c4c8044a831011
RakanAja05 <muhammadrakanhibrizi2005@mail.ugm.ac.id>
2026-09-08 21:03:20 +0700

    feat: tambah route dan controller halaman profil

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ProfileController extends Controller
+{
+    public function show()
+    {
+        $profile = [
+            'nama'  => 'Muhammad Rakan Hibrizi',
+            'nim'   => '24/539132/SV/24568',
+            'kelas' => 'Evolusi Perangkat Lunak',
+        ];
+
+        return view('profile', compact('profile'));
+    }
+}
